98ccfdcbf61553d6feff22ceb22e352a34edcfab
Oliwier <oliwierrosiak07@gmail.com>
2026-03-03 18:04:57 +0100

    Added dev and prod method of server start

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
-console.log("abc")
+import dotenv from 'dotenv'
+import app from "./server/expressConfig.js";
+
+dotenv.config({
+    path:`.env.${process.env.NODE_ENV}`
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server listen on port ${process.env.PORT}`)
+})
