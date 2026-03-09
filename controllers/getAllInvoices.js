25d5a09889c200862ac575164f510dfa0d2bb41d
Oliwier <oliwierrosiak07@gmail.com>
2026-03-09 23:03:04 +0100

    Added invoices pagination in getting data fomr db

--- controllers/getAllInvoices.js
+++ controllers/getAllInvoices.js
@@ -4,8 +4,15 @@ async function getAllInvoices(req,res)
 {
     try
     {
-        const invoices = await Invoices.find({},'ksefNumber Fa.NumerFaktury Fa.Waluta Fa.DataWystawienia Podsumowanie.Brutto action').sort({'Fa.DataWystawienia':1})
-        res.status(200).json(invoices)
+        const invoices = await Invoices.find({},'ksefNumber Fa.NumerFaktury Fa.Waluta Fa.DataWystawienia Podsumowanie.Brutto action').skip(req.query.skip*10).limit(10).sort({'Fa.DataWystawienia':-1})
+        const returnObject = {invoices:invoices}
+        if(req.query.skip == 0)
+        {
+            const fullInvoiceLength = await Invoices.find({},'_id').countDocuments()
+            returnObject.maxLength = fullInvoiceLength
+        }
+        if(!invoices.length) throw new Error()
+        res.status(200).json(returnObject)
     }
     catch(ex)
     {
