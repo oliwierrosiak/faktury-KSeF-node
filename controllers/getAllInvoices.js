de6e4ecb29b95f82f7c72b654766a376367f40cf
Oliwier <oliwierrosiak07@gmail.com>
2026-03-10 00:13:09 +0100

    Added search invoices in DB by month

--- controllers/getAllInvoices.js
+++ controllers/getAllInvoices.js
@@ -4,18 +4,43 @@ async function getAllInvoices(req,res)
 {
     try
     {
-        const invoices = await Invoices.find({},'ksefNumber Fa.NumerFaktury Fa.Waluta Fa.DataWystawienia Podsumowanie.Brutto action').skip(req.query.skip*10).limit(10).sort({'Fa.DataWystawienia':-1})
+        let invoices, startDate, endDate;
+        if(req.query.date == 'all')
+        {
+            invoices = await Invoices.find({},'ksefNumber Fa.NumerFaktury Fa.Waluta Fa.DataWystawienia Podsumowanie.Brutto action').skip(req.query.skip*10).limit(10).sort({'Fa.DataWystawienia':-1})
+        }
+        else
+        {
+            const startDateArray = req.query.date.split('-')
+            const endDateArray = req.query.date.split('-')
+            startDate = `${startDateArray[0]}-${startDateArray[1].length === 1?`0${startDateArray[1]}`:startDateArray[1]}-${startDateArray[2]}`
+            endDate = `${endDateArray[0]}-${+endDateArray[1]+1 > 9?+endDateArray[1]+1:`0${+endDateArray[1]+1}`}-${endDateArray[2]}`
+            invoices = await Invoices.find({'Fa.DataWystawienia':{$gte:startDate,$lt:endDate}})
+        }
+
+        if(!invoices.length) throw new Error()
+
         const returnObject = {invoices:invoices}
+
         if(req.query.skip == 0)
         {
-            const fullInvoiceLength = await Invoices.find({},'_id').countDocuments()
+            let fullInvoiceLength = 0
+            if(req.query.date == 'all')
+            {
+                fullInvoiceLength = await Invoices.find({},'_id').countDocuments()
+            }
+            else
+            {
+                fullInvoiceLength = await Invoices.find({'Fa.DataWystawienia':{$gte:startDate,$lt:endDate}}).countDocuments()
+            }
             returnObject.maxLength = fullInvoiceLength
         }
-        if(!invoices.length) throw new Error()
+
         res.status(200).json(returnObject)
     }
     catch(ex)
     {
+        console.log(ex)
         res.sendStatus(404)
     }
 }
