2b5692d699b417ebef1039475671691debc7d927
Oliwier <oliwierrosiak07@gmail.com>
2026-03-10 19:50:28 +0100

    Added advanced search query to mongoDB

--- controllers/search.js
+++ controllers/search.js
@@ -4,7 +4,7 @@ async function search(req,res)
 {
     try
     {
-        const list = await Invoices.find({},'Fa.NumerFaktury Fa.DataWystawienia').limit(10)
+        const list = await Invoices.find({$or:[{"Fa.NumerFaktury":{$regex:`^${req.query.query}`, $options:'i'}},{ksefNumber:{$regex:`^${req.query.query}`,$options:'i'}},{"Fa.DataWystawienia":{$regex:`^${req.query.query}`}}]}).select("Fa.NumerFaktury Fa.DataWystawienia").sort({'Fa.DataWystawienia':-1})
         res.status(200).json(list)
     }
     catch(ex)
