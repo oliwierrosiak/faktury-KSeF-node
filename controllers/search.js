123bf04df9ea3c676ef7824f4c03ee1593810409
Oliwier <oliwierrosiak07@gmail.com>
2026-03-10 13:42:39 +0100

    Added search route

---
+++ controllers/search.js
@@ -0,0 +1,16 @@
+import { Invoices } from "../db/dbConfig.js"
+
+async function search(req,res)
+{
+    try
+    {
+        const list = await Invoices.find({},'Fa.NumerFaktury Fa.DataSprzedazy').limit(10)
+        res.status(200).json(list)
+    }
+    catch(ex)
+    {
+        res.sendStatus(404)
+    }
+}
+
+export default search
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import downloadInvoices from "../controllers/downloadInvoices.js";
 import invoiceActionUpdate from "../controllers/invoiceActionUpdate.js";
 import getInvoices from "../controllers/getInvoices.js";
+import search from "../controllers/search.js";
 
 const router = new Router()
 
@@ -10,5 +11,7 @@ router.get('/downloadInvoices',downloadInvoices)
 router.get('/getInvoices',getInvoices)
 
 router.post('/invoiceActionUpdate',invoiceActionUpdate)
+
+router.get('/search',search)
 
 export default router
