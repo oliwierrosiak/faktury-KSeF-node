2852f0498f0d74acc3794b37388098a26a2269c3
Oliwier <oliwierrosiak07@gmail.com>
2026-03-10 20:36:38 +0100

    Added throwing error when none invoices are found

--- controllers/search.js
+++ controllers/search.js
@@ -5,6 +5,7 @@ async function search(req,res)
     try
     {
         const list = await Invoices.find({$or:[{"Fa.NumerFaktury":{$regex:`^${req.query.query}`, $options:'i'}},{ksefNumber:{$regex:`^${req.query.query}`,$options:'i'}},{"Fa.DataWystawienia":{$regex:`^${req.query.query}`}}]}).select("Fa.NumerFaktury Fa.DataWystawienia").sort({'Fa.DataWystawienia':-1})
+        if(!list.length) throw new Error()
         res.status(200).json(list)
     }
     catch(ex)
