56c997325916efd0f786f27805871fdc50ee3e31
Oliwier <oliwierrosiak07@gmail.com>
2026-03-16 23:02:48 +0100

    Realized primary structure of getting invoices, added 3 months date support

--- controllers/downloadInvoices.js
+++ controllers/downloadInvoices.js
@@ -9,8 +9,6 @@ async function downloadInvoices(req,res)
     {
         const {token,number} = await ksefAuth()
 
-        if(!token) throw new Error()
-
         const auth = await awaitUntilAuthBeDone(number,token)
 
         if(auth === false)
@@ -20,12 +18,26 @@ async function downloadInvoices(req,res)
 
         const accessToken = await getKsefTokens(token) 
 
-
+        const startDate = new Date()
+        startDate.setMonth(startDate.getMonth() - 3)
+        startDate.setDate(startDate.getDate()+1)
+        
+        const invoicesMeta = await axios.post(`${process.env.KSEF}/invoices/query/metadata?pageSize=250`,{
+            subjectType:'Subject2',
+            dateRange:{
+                dateType:'Issue',
+                from:startDate
+            }
+        },
+        {headers:{"Authorization":`Bearer ${accessToken}`}}
+        )
+        console.log(invoicesMeta.data.hasMore)
+        console.log(invoicesMeta.data.invoices.length)
         res.sendStatus(200)
     }
     catch(ex)
     {
-        console.log(ex)
+        console.log(ex.response.data.exception.exceptionDetailList)
         res.sendStatus(500)
     }
    
